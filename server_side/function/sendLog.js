155424bdc5db49014c4f7a87c52a5e6a7a670701
Muhammad Tier Sinyo C.U.S. <118105588+Stalker-moment@users.noreply.github.com>
2024-07-14 16:31:11 +0700

    Update sendLog.js

--- server_side/function/sendLog.js
+++ server_side/function/sendLog.js
@@ -6,8 +6,24 @@ async function sendLog(reverse) {
   try {
     const data = fs.readFileSync(filePath, "utf8");
     const dataJson = JSON.parse(data);
-    const stringifyData = JSON.stringify(dataJson, null, 2);
-    return stringifyData;
+
+    //extract duration progress
+    let progressData = dataJson.duration.progress.split("/");
+    let progress = progressData[0].split(":");
+
+    const isPlaying = progress !== previousDuration;
+    previousDuration = progress;
+
+    if (isPlaying) {
+      const stringifyData = JSON.stringify(dataJson, null, 2);
+      return stringifyData;
+    } else {
+      //write the data to json file if is_playing is false
+      fs.writeFileSync(filePath, JSON.stringify(dataJson, null, 2));
+      const stringifyData = JSON.stringify(dataJson, null, 2);
+      return stringifyData;
+    }
+    
   } catch (error) {
     return { error: error };
   }
